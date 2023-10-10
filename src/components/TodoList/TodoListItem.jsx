76e31c2dd7585cfbe8e589b420f7c16c5aaf53b6
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-10-10 17:43:48 +0200

    added checked feather

--- src/components/TodoList/TodoListItem.jsx
+++ src/components/TodoList/TodoListItem.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { AiOutlineDelete } from 'react-icons/ai';
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 import css from './TodoList.module.css';
 
-const TodoListItem = ({ todo, deleteTodoItem }) => {
+const TodoListItem = ({ todo, deleteTodoItem, onChange, changeTodoItem }) => {
   return (
     <div key={todo.id} className={css.item}>
       <label className={css.itemlabel}>
-        <input type="checkbox" name="hasTodo" />
+        <input
+          type="checkbox"
+          name="hasTodo"
+          checked={todo.checked}
+          onChange={e => {
+            onChange(todo.id, e.target.checked);
+          }}
+        />
+
         {todo.text}
       </label>
-      <button onClick={() => deleteTodoItem(todo.id)} className={css.itembtn}>
-        <AiOutlineDelete />
-      </button>
+      <div>
+        <button className={css.itembtn} ooClick={() => changeTodoItem(todo.id)}>
+          <AiOutlineEdit />
+        </button>
+        <button onClick={() => deleteTodoItem(todo.id)} className={css.itembtn}>
+          <AiOutlineDelete className={css.icon_delete} />
+        </button>
+      </div>
     </div>
   );
 };
